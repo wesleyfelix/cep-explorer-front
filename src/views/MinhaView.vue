147331148fee7b7d5f5d4beea8190c6b69f38f65
wesleyfelix <wesley.felixdelima96@hotmail.com>
2023-11-20 21:29:40 -0300

    iniciando o front

---
+++ src/views/MinhaView.vue
@@ -0,0 +1,30 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <!-- Cabeçalho -->
+    <q-header elevated>
+      <q-toolbar>
+        <q-toolbar-title>Meu Aplicativo Quasar</q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <!-- Conteúdo da Página -->
+    <q-page-container>
+      <router-view /> <!-- Este é o ponto onde a sua view será renderizada -->
+    </q-page-container>
+
+    <!-- Rodapé -->
+    <q-footer elevated>
+      <!-- Conteúdo do rodapé, se necessário -->
+    </q-footer>
+  </q-layout>
+</template>
+
+<script>
+export default {
+  name: 'AppLayout',
+};
+</script>
+
+<style scoped>
+/* Adicione estilos conforme necessário */
+</style>
